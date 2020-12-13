543dbff2d38f14be75d667d7f999256dce2c8b15
Michal <michalwit111@gmail.com>
2020-12-13 11:10:33 +0100

    Move hosts configuration to application.yml

--- apps/mediator/src/main/java/org/example/actuator/MediatingController.java
+++ apps/mediator/src/main/java/org/example/actuator/MediatingController.java
@@ -1,6 +1,8 @@
 package org.example.actuator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
@@ -15,13 +17,16 @@ public class MediatingController {
 	@Autowired
 	private WebClient webClient;
 
+	@Autowired
+	private Config config;
+
 	@RequestMapping("/call-actuator-service")
 	public String callActuatorService() {
 		final String restTemplateResp = restTemplate
-				.getForObject("http://actuator/hello", String.class);
+				.getForObject("http://" + config.getHost() + "/hello", String.class);
 		final String webClientResp = webClient
 				.get()
-				.uri("http://actuator/hello")
+				.uri("http://" + config.getHost() + "/hello")
 				.exchange()
 				.block()
 				.bodyToMono(String.class)
@@ -31,5 +36,20 @@ public class MediatingController {
 		System.out.println("WebClient response: " + webClientResp);
 
 		return restTemplateResp + "  |  " + webClientResp;
+	}
+
+	@Configuration
+	@ConfigurationProperties(prefix = "actuator-service")
+	static class Config {
+
+		private String host;
+
+		public String getHost() {
+			return host;
+		}
+
+		public void setHost(String host) {
+			this.host = host;
+		}
 	}
 }
--- apps/spring-cloud-gateway/src/main/java/gateway/Application.java
+++ apps/spring-cloud-gateway/src/main/java/gateway/Application.java
@@ -1,11 +1,12 @@
 package gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Application {
+
+	@Autowired
+	private Config config;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -29,7 +33,7 @@ public class Application {
 						.addRequestHeader("X-Gateway", "custom-header")
 						.rewritePath("routed", "actuator")
 				)
-				.uri("http://actuator:80")
+				.uri("http://" + config.getActuatorHost())
 			)
 			.route(p -> p
 				.path("/mediator")
@@ -37,7 +41,7 @@ public class Application {
 						.addRequestHeader("X-Gateway", "_custom_header_")
 						.rewritePath("mediator", "call-actuator-service")
 				)
-				.uri("http://mediator:80")
+				.uri("http://" + config.getMediatorHost())
 			)
 			.build();
 	}
@@ -45,5 +49,29 @@ public class Application {
 	@RequestMapping("/fallback")
 	public Mono<String> fallback() {
 		return Mono.just("fallback");
+	}
+
+	@Configuration
+	@ConfigurationProperties(prefix = "services")
+	static class Config {
+
+		private String actuatorHost;
+		private String mediatorHost;
+
+		public String getActuatorHost() {
+			return actuatorHost;
+		}
+
+		public void setActuatorHost(String actuatorHost) {
+			this.actuatorHost = actuatorHost;
+		}
+
+		public String getMediatorHost() {
+			return mediatorHost;
+		}
+
+		public void setMediatorHost(String mediatorHost) {
+			this.mediatorHost = mediatorHost;
+		}
 	}
 }
